641e5e7d7972097c7e7d319380435f122e820242
irene <sykim1123@nate.com>
2020-09-18 10:55:39 +0900

    css와 images 폴더 추가 하고 부트스트랩 사용해보기.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React ,{ Component } from 'react';
 //이미지 경로를 import 해서 사용한다.
 import logo from './logo.svg';
+import kim from './images/kim1.png'
 //css파일을 import 만 해도 css가 자동 적용 된다.
+import './css/bootstrap.css';
 import './App.css';
 
-function App() {
-  const myName="김구라";
-  return (
-    <div>
-      <h1>Hello React!</h1>
-      <p>내이름은 <strong>{myName}</strong></p>
-      <img src={logo} alt="react js 로고 입니다"/>
+
+//class 표현식으로 App구성하기
+class App extends Component {
+  //App.js를 이용해서 화면 구성을 할때 호출되는 메소드
+  render() {
+    const myName="김구라";
+    return (
+      <div className="container">
+        {/* class는 예약어이기 때문에 className을 사용한다. */}
+        <h1>Hello React!</h1>
+        <p>내이름은 <strong>{myName}</strong></p>
+        <img src={logo} alt="react js 로고 입니다"/>
+        <img src={kim} alt="김구라 이미지" />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
