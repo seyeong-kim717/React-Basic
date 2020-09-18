41f18f81f7d7d263cf9f658034ae2aef6ea8efe9
irene <sykim1123@nate.com>
2020-09-18 10:27:13 +0900

    첫번째 commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//이미지 경로를 import 해서 사용한다.
+import logo from './logo.svg';
+//css파일을 import 만 해도 css가 자동 적용 된다.
+import './App.css';
+
+function App() {
+  const myName="김구라";
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>내이름은 <strong>{myName}</strong></p>
+      <img src={logo} alt="react js 로고 입니다"/>
+    </div>
+  );
+}
+
+export default App;
